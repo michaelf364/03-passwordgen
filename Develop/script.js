02de7e55175e5e2667529d5cb571823deb9de535
Michael Farris <farris.michael.j@gmail.com>
2020-04-13 23:08:24 -0700

    basically created the password generator but need to display the password

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,48 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordLength = 0;
+var charset = { lowercase: "abcdefghijklmnopqrstuvwxyz",
+ uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ,
+ numbers: "0123456789" ,
+ characters: "!@#$%^&*()_+~`|}{[]\:;?><,./-=" };
+var useLowercase = true;
+var useUppercase = true;
+var useNumbers = true;
+var useCharacters = true;
+var passwordOptions = "";
+var genPassword = "";
+passwordLength = prompt("how long should the password be?");
+while ((passwordLength < 8) || (passwordLength > 128)) {
+  alert("Password must be between 8 and 128 characters long!");
+  passwordLength = prompt("How long should the password be?");
+}
+console.log(passwordLength);
+useLowercase = confirm("Do you want to include lowercase characters?");
+console.log(useLowercase);
+useUppercase = confirm("Do you want to include uppercase characters?");
+console.log(useUppercase);
+useNumbers = confirm("Do you want to include numbers?");
+console.log(useNumbers);
+useCharacters = confirm("Do you want to include special characters?");
+console.log(useCharacters);
+if (useLowercase) {
+  passwordOptions = charset.lowercase;
+} 
+if (useUppercase) {
+passwordOptions = passwordOptions + charset.uppercase;
+} 
+if (useNumbers) {
+  passwordOptions = passwordOptions + charset.numbers;
+} 
+if (useCharacters) {
+  passwordOptions = passwordOptions + charset.characters;
+} 
+for (let i = 0, n = passwordLength; i < passwordLength; i++) {
+  genPassword += passwordOptions.charAt(Math.floor(Math.random() * n ));
+}
 
+console.log(passwordOptions);
+console.log(genPassword);
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
